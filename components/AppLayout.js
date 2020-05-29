a499cbd363b984a25929102cbd9a1fb3ed30c584
박종열/강남커넥츠팀 <jongyeol@stunitas.com>
2020-05-29 19:14:53 +0900

    intro next.js and layout

---
+++ components/AppLayout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Input, Menu, Button} from "antd";
+import Link from 'next/link';
+
+const AppLayout = ({children}) => {
+    return (
+        <div>
+            <Menu mode={"horizontal"}>
+                <Menu.Item key={"home"}><Link href={"/"}><a>발짝</a></Link></Menu.Item>
+                <Menu.Item key={"menu"}><Link href={"/menu"}><a>메뉴소개</a></Link></Menu.Item>
+                <Menu.Item key={"mail"}>
+                    <Input.Search enterButton style={{verticalAlign: 'middle'}}/>
+                </Menu.Item>
+            </Menu>
+            <Button><Link href={"/user/signup"}><a>회원가입</a></Link></Button>
+            {children}
+        </div>
+    )
+}
+export default AppLayout;
