ff80688ecc9ac924f9fa9dcf5af717e95e38b584
jongyeol91 <pjr159@gmail.com>
2020-06-02 19:57:40 +0900

    LoginForm 양식

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import {Input, Menu, Button} from "antd";
+import {Form, Input, Menu, Button, Row, Col, Card, Avatar} from "antd";
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import LoginForm from "./LoginForm";
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 
+const dummy = {
+    name: "박종열",
+    post: ["1", "2", "3"],
+    isLoggedIn: false,
+}
 
 const AppLayout = ({ children }) => {
     return (
@@ -15,7 +22,28 @@ const AppLayout = ({ children }) => {
                 </Menu.Item>
             </Menu>
             <Button><Link href={"/user/signup"}><a>회원가입</a></Link></Button>
-            {children}
+            <Row>
+                <Col xs={24} md={6}>
+                    {dummy.isLoggedIn ?
+                        <Card actions={[
+                            <SettingOutlined key="setting"/>,
+                            <EditOutlined key="edit"/>,
+                            <EllipsisOutlined key="ellipsis"/>,
+                        ]}>
+                            <Card.Meta
+                                avatar={<Avatar
+                                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
+                                title={dummy.name}
+                                description="This is the description"/>
+                        </Card>
+                        :
+                        <LoginForm/>
+                    }
+                </Col>
+                <Col xs={24} md={12}> {children}</Col>
+                <Col xs={24} md={6}></Col>
+
+            </Row>
         </div>
     )
 }
