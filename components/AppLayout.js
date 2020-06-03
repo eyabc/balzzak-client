4f92d86526f24ea292310fe5261a9ed9ff8e5c40
jongyeol91 <pjr159@gmail.com>
2020-06-03 18:25:42 +0900

    kakao 지도 적용

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -17,12 +17,13 @@ const AppLayout = ({ children }) => {
             <Menu mode={"horizontal"}>
                 <Menu.Item key={"home"}><Link href={"/"}><a>발짝</a></Link></Menu.Item>
                 <Menu.Item key={"menu"}><Link href={"/menu"}><a>메뉴소개</a></Link></Menu.Item>
+                <Menu.Item key={"location"}><Link href={"/location"}><a>오시는길</a></Link></Menu.Item>
                 <Menu.Item key={"mail"}>
                     <Input.Search enterButton style={{verticalAlign: 'middle'}}/>
                 </Menu.Item>
             </Menu>
             <Button><Link href={"/user/signup"}><a>회원가입</a></Link></Button>
-            <Row>
+            <Row gutter={20}>
                 <Col xs={24} md={6}>
                     {dummy.isLoggedIn ?
                         <Card actions={[
