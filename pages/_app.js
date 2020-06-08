5ad919b990a9e111b845c36dba89aa74c1770a5f
jongyeol91 <pjr159@gmail.com>
2020-06-08 19:37:06 +0900

    reducer합치기, next-redux-wrapper적용

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Head from "next/head";
 import PropTypes from 'prop-types';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import AppLayout from "../components/AppLayout";
+import withRedux from 'next-redux-wrapper';
+import reducer from '../reducers';
 
-const Balzzak = ({ Component }) => {
+const Balzzak = ({ Component, store }) => {
     return (
-        <>
+        <Provider store={store}>
             <Head>
                 <title>발짝</title>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.2.5/antd.compact.css"/>
@@ -13,12 +17,17 @@ const Balzzak = ({ Component }) => {
             <AppLayout>
                 <Component/>
             </AppLayout>
-        </>
+        </Provider>
     )
 }
 
 Balzzak.prototypes = {
     Component: PropTypes.elementType, // myComponent
+    store: PropTypes.object
 }
 
-export default Balzzak;
+// 발짝에 store을 넣어주는 higher order component.
+export default withRedux((initialState, options) => {
+    const store = createStore(reducer, initialState);
+    return store;
+})(Balzzak);
