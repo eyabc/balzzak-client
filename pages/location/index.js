4754501eba98441ff8635e25ba40855f7f160330
Jongyeol Park <34529589+Jongyeol91@users.noreply.github.com>
2021-01-05 21:05:13 +0900

    [개행 충돌] #18 매장정보 페이지 -> 지역 검색 -> 커스텀 드롭다운 훅 적용 (#25)

--- pages/location/index.js
+++ pages/location/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Row, Col } from 'antd';
 import LocationTap from './tab';
 import MapContent from './location';
+import LocationSearch from './locationSearch';
 
 const Location = () => {
   return (
@@ -14,7 +15,10 @@ const Location = () => {
       <Row>
         <Col span={8}>
           <LocationTap />
-          <div style={{ border: '1px solid black', height: '600px' }}>임시 정보</div>
+          <div style={{ border: '1px solid black', height: '600px' }}>
+            <LocationSearch />
+            임시 정보
+          </div>
         </Col>
         <Col span={10}>
           <MapContent />
