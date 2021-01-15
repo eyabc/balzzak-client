e207ac529fb47ad7d54461b52d227a2cb1d9a582
Jihye <kimji9663@nate.com>
2021-01-15 16:02:15 +0900

    메뉴 antd 레이아웃 뺌

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import {
-  Layout, Menu,
-} from 'antd';
+import styled from "@emotion/styled";
+import { Layout, Row, Col } from 'antd';
 import Link from 'next/link';
 
-const logoImage = 'images/logo_color.png';
+const logoImage = 'images/balzzak_logo.png';
 const { Header, Footer, Content } = Layout;
 
-const menuItem = {
-  color: '#fff',
-}
+const StyledHeader = styled(Header)` 
+  position: relative;
+  z-index: 1;
+  padding: 0;
+  height: auto;
+  border-bottom: 1px solid #fff;
+  background: none;
+`;
+const StyledMenu = styled('ul')`
+  display: flex;
+  flex-flow: row-reverse;
+  margin-bottom: 0;
+`;
+
+const MenuItem = styled('li')`
+  float: left;
+  padding: 0 3%;
+  list-style: none;
+
+  & > a {
+    color: #fff;
+    font-size: 20px;
+    background: rgba(255, 255, 255, .2);
+  }
+`;
 
 const AppLayout = ({ children }) => (
   <>
@@ -18,52 +39,42 @@ const AppLayout = ({ children }) => (
         width: '100%',
       }}
     >
-      <Header
-        style={{
-          position: 'relative',
-          zIndex: '1',
-          padding: 0,
-          height: 'auto',
-          borderBottom: '1px solid #fff',
-          background: 'none',
-        }}
-      >
-        <div
+      <StyledHeader>
+        <Row
           style={{
             display: 'flex',
             maxWidth: 1140,
             height: 100,
             margin: '0 auto',
             justifyContent: 'space-between',
-            alignItems: 'center',
+            alignItems: 'flex-end',
           }}
         >
-          <Link href="/">
-            <img
-              src={logoImage}
-              alt="logo"
-              style={{
-                width: 200,
-                cursor: 'pointer',
-                filter: 'invert(1)',
-              }}
-            />
-          </Link>
-          <Menu
-            mode="horizontal"
-            style={{
-              background: 'none',
-              color: '#fff',
-            }}
-          >
-            <Menu.Item key="menu" style={menuItem}><Link href="/menu">메뉴소개</Link></Menu.Item>
-            <Menu.Item key="location" style={menuItem}><Link href="/location">매장정보</Link></Menu.Item>
-            <Menu.Item key="2" style={menuItem}><Link href="/2">이벤트 및 제휴</Link></Menu.Item>
-            <Menu.Item key="3" style={menuItem}><Link href="/3">창업 안내</Link></Menu.Item>
-            <Menu.Item key="4" style={menuItem}><Link href="/4">고객의 소리</Link></Menu.Item>
-          </Menu>
-        </div>
-      </Header>
+          <Col span={4}>
+            <Link href="/">
+                <img
+                  src={logoImage}
+                  alt="logo"
+                  style={{
+                    width: 81,
+                    cursor: 'pointer',
+                    paddingBottom: 13,
+                  }}
+                />
+            </Link>
+          </Col>
+          <Col span={20}>
+            <StyledMenu>
+                <MenuItem><Link href="/menu">메뉴소개</Link></MenuItem>
+                <MenuItem><Link href="/location">매장정보</Link></MenuItem>
+                <MenuItem><Link href="/2">이벤트 및 제휴</Link></MenuItem>
+                <MenuItem><Link href="/3">창업 안내</Link></MenuItem>
+                <MenuItem><Link href="/4">고객의 소리</Link></MenuItem>
+                <MenuItem><Link href="/5">커뮤니티</Link></MenuItem>
+            </StyledMenu>
+          </Col>
+        </Row>
+      </StyledHeader>
       <Content>
         {children}
       </Content>
