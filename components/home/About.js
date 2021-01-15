c4ba181226226e920e23598ad2ca43ee8815605a
Jihye Kim <37232512+kimji9663@users.noreply.github.com>
2021-01-15 13:01:11 +0900

    main 퍼블리싱 진행중 (#28)
    * main 퍼블리싱 진행중
    
    * Merge master
    
    * emotion설치, styled-component제거
    
    * 매장정보 페이지 -> 컨텐츠 가운데정렬
    
    * 레이아웃 페이지 -> 메인 페이지만 헤더가 겹치도록 수정
    
    * Main - About 레이아웃 진행중
    
    * main 퍼블리싱 진행중
    
    Co-authored-by: Jihye <kimji9663@nate.com>

--- components/home/About.js
+++ components/home/About.js
@@ -17,15 +17,31 @@ const BreifInfo = styled.div`
   font-weight: bold;
   text-align: center;
 `;
+const AboutLinkWrap = styled(Row)`
+  margin-top: 50px;
+`;
 const TitleRight = styled.div`
   position: absolute;
-  top: 0;
-  right: 0;
-  font-size: 14px;
+  top: 10px;
+  right: 10px;
+  font-weight: bold;
+  font-size: 22px;
   color: #fff;
 `;
 const TitleLeft = styled.div`
-  font-size: 14px;
+  color: #FF8913;
+  font-weight: bold;
+  font-size: 22px;
+`;
+const RightRow = styled(Row)`
+  background: #fff;
+  cursor: pointer;
+`;
+const RightCol = styled(Col)`
+  padding: 23px 18px;
+`;
+const ContInner = styled.div`
+  font-size: 20px;
 `;
 
 const About = () => {
@@ -37,30 +53,36 @@ const About = () => {
         발짝은 한번 머물고 가는 곳이 아닌, 사람들과 추억을 만들고, 그 추억이 이야기가 되는 공간입니다. <br/>
         찾아와 주시는 모든 분들이 편하고 안락하게 즐길 수 있도록 정성껏 대접하는 포차가 되겠습니다.
       </BreifInfo>
-      <Row>
+      <AboutLinkWrap>
         <Col span={10}>
           <Link href="/menu"><img src={imgMenu} style={{width: "100%"}}/></Link>
           <TitleRight>추천메뉴</TitleRight>
         </Col>
         <Col span={14} style={{paddingLeft: "3%"}}>
-          <Row>
-            <Col span={10}>
-              <Link href="/location"><img src={imgMap} style={{width: "100%"}}/></Link>
-            </Col>
-            <Col span={14}>
-              <TitleLeft>매장찾기</TitleLeft>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={10}>
-              <Link href="/3"><img src={imgPartner} style={{width: "100%"}}/></Link>
-            </Col>
-            <Col span={14}>
-              <TitleLeft>전수창업문의</TitleLeft>
-            </Col>
-          </Row>
+          <Link href="/location">
+            <RightRow>
+                <Col span={10}>
+                  <img src={imgMap} style={{width: "100%"}}/>
+                </Col>
+                <RightCol span={14}>
+                  <TitleLeft>매장찾기</TitleLeft>
+                  <ContInner>근처에 있는 발짝 매장을 찾아보세요! 한 잔 술, 맛있는 안주와 함께하는 하루의 마무리는 발짝에서~!</ContInner>
+                </RightCol>
+            </RightRow>
+          </Link>
+          <Link href="/3">
+            <RightRow style={{marginTop: "5%"}}>
+              <Col span={10}>
+                <img src={imgPartner} style={{width: "100%"}}/>
+              </Col>
+              <RightCol span={14}>
+                <TitleLeft>전수창업문의</TitleLeft>
+                <ContInner>작은고추가 맵다! 백마진 없음! 상생하는 발짝!</ContInner>
+              </RightCol>
+            </RightRow>
+          </Link>
         </Col>
-      </Row>
+      </AboutLinkWrap>
     </AboutUs>
     </>
   );
