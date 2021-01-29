2f2bd11264ef37b5f9c34c8526471a838be18043
Jihye <kimji9663@nate.com>
2021-01-29 19:25:46 +0900

    메인 소개, SNS 영역 반응형 구현

--- components/home/SNSTag.js
+++ components/home/SNSTag.js
@@ -5,6 +5,10 @@ import Link from 'next/link';
 
 const imgSample = '../images/sample01.png';
 
+const BREAK_POINT_MOBILE = 768;
+const BREAK_POINT_TABLET = 992;
+const BREAK_POINT_PC = 1200;
+
 const Instagram = styled.div`
   max-width: 1140px;
   margin: 0 auto;
@@ -15,14 +19,24 @@ const Instagram = styled.div`
     font-size: 30px;
     text-align: center;
   }
+  & ul {
+    padding: 0;
+  }
   & li {
       float: left;
       padding: 5px;
-      width: 14.28%;
+      width: 50%;
       list-style: none;
   }
   & li img {
       width: 100%;
+  }
+
+  @media only screen and (min-width: ${BREAK_POINT_MOBILE}px) {  
+    & li {
+        padding: 5px;
+        width: 14.28%;
+    }
   }
 `;
 
