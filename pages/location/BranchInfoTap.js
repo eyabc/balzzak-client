c89ab76159061aced4ec570095abb545b4dd1713
jongyeol park <pjr159@gmail.com>
2021-02-07 00:27:20 +0900

    [#18] 매장정보 지점 탭 레이아웃
    앤트디자인 Card 활용
    탭 컴포넌트 별도 분리

---
+++ pages/location/BranchInfoTap.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card, Button } from 'antd';
+
+const BranchInfoTap = () => (
+  <>
+    <Card title="1호점" style={{ width: 300 }}>
+      <p>중랑점</p>
+      <p>서울 중랑구 중랑역로 73 1층</p>
+      <Button>상세보기</Button>
+    </Card>
+  </>
+);
+
+export default BranchInfoTap;
